ec644933246c37064580016433639ec134d2bc68
Martin <maguera37@gmail.com>
2026-02-16 16:26:01 -0300

    feat: added ProjectsController, Project model, and associated tests and fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_07_201328) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_08_184634) do
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "todos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
