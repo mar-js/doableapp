adffaa8158b405badcd48cb3c0253eedcd800512
Martin <maguera37@gmail.com>
2026-04-08 17:49:38 -0300

    feat: implemented create action for projects with validation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,4 +10,19 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new()
   end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
