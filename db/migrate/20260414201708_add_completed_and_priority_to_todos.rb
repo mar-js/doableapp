6752d3951505303196624ce79ed28a0969f9a296
Martin <maguera37@gmail.com>
2026-04-14 17:20:07 -0300

    feat: setted default values and constraints for completed and priority columns in todos

--- db/migrate/20260414201708_add_completed_and_priority_to_todos.rb
+++ db/migrate/20260414201708_add_completed_and_priority_to_todos.rb
@@ -1,6 +1,6 @@
 class AddCompletedAndPriorityToTodos < ActiveRecord::Migration[8.1]
   def change
-    add_column :todos, :completed, :boolean
-    add_column :todos, :priority, :integer
+    add_column :todos, :completed, :boolean, default: false, null: false
+    add_column :todos, :priority, :integer, default: false, null: false
   end
 end
