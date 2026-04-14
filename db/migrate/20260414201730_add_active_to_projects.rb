139f657bacd33b517f49a8649e169e7a044294e9
Martin <maguera37@gmail.com>
2026-04-14 17:20:43 -0300

    fix: updated default value for active column in projects to true

--- db/migrate/20260414201730_add_active_to_projects.rb
+++ db/migrate/20260414201730_add_active_to_projects.rb
@@ -1,5 +1,5 @@
 class AddActiveToProjects < ActiveRecord::Migration[8.1]
   def change
-    add_column :projects, :active, :boolean, default: false, null: false
+    add_column :projects, :active, :boolean, default: true, null: false
   end
 end
