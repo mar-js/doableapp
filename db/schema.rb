1f7f787a0f8601e6be175cef4c42aaa69a998e7e
Martin <maguera37@gmail.com>
2026-04-14 17:22:10 -0300

    feat: updated the schema version and defined default values ​​for the active column in projects and the completed and priority columns in all

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_08_184634) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_14_201730) do
   create_table "projects", force: :cascade do |t|
+    t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "todos", force: :cascade do |t|
+    t.boolean "completed", default: false, null: false
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name"
+    t.integer "priority", default: 0, null: false
     t.datetime "updated_at", null: false
   end
 end
