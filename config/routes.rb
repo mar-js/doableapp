23909159c670232b2ba54f7cf6e863bab6377299
Martin <maguera37@gmail.com>
2026-04-19 16:41:05 -0300

    feat: defined root path route for pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#home"
   resources :todos
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
