eb0477f03385c325126cee3fc14c49e02612c7e9
Martin <maguera37@gmail.com>
2026-04-23 16:36:16 -0300

    feat: added session and password resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
+  resource :session
+  resources :passwords, param: :token
   resources :todos
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
