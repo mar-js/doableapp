080bcfdd72e6396363036eb21f268309cbb98ce0
Martin <maguera37@gmail.com>
2026-04-26 16:36:29 -0300

    fix: corrected typo in the dependent option for project association in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :projects, dependent: :detroy
+  has_many :projects, dependent: :destroy
   has_many :sessions, dependent: :destroy
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
