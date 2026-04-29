c94f252de207286f0f30cf4bcab3c98adec1bb58
Martin <maguera37@gmail.com>
2026-04-29 16:42:25 -0300

    feat: implemented new user registration actions in RegistersController

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,3 +1,25 @@
 class RegistersController < ApplicationController
   skip_before_action :require_authentication, only: [ :new, :create ]
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.create(register_params)
+
+    if @user.save
+      reset_session
+      session[:user_id] = @user.id
+      flash[:notice] = "Project REGISTER successfully!"
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def register_params
+      params.require(:user).permit(:email_address, :password)
+    end
 end
