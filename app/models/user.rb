1a7c41305ab9e6db5c7c0fde1a934aedf042c40b
Martin <maguera37@gmail.com>
2026-05-01 17:09:11 -0300

    feat: added association for todos through projects in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :projects, dependent: :destroy
+  has_many :todos, through: :projects
   has_many :sessions, dependent: :destroy
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
