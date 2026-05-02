af0d0cba7012019d46847754c4a89bde7fa2abd7
Martin <maguera37@gmail.com>
2026-05-02 17:09:34 -0300

    feat: refactoed TodosController to improve todo management and flash messages

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,70 +1,60 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :require_authentication
+  before_action :set_project, only: %i[index new create]
+  before_action :set_todo, only: %i[show edit update destroy]
 
-  # GET /todos or /todos.json
   def index
-    @todos = Todo.all
+    @todos = @project.todos.includes(:project).order(created_at: :desc)
   end
 
-  # GET /todos/1 or /todos/1.json
   def show
   end
 
-  # GET /todos/new
   def new
-    @todo = Todo.new
+    @todo = @project.todos.new
   end
 
-  # GET /todos/1/edit
+  def create
+    @todo = @project.todos.build(todo_params)
+
+    if @todo.save
+      flash[:notice] = "Todo CREATED successfully!"
+      redirect_to project_todo_path(@project, @todo)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit
   end
 
-  # POST /todos or /todos.json
-  def create
-    @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /todos/1 or /todos/1.json
   def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: "Todo was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo UPDATED successfully!"
+      redirect_to project_todo_path(@todo.project, @todo)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /todos/1 or /todos/1.json
   def destroy
-    @todo.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to todos_path, notice: "Todo was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    project = @todo.project
+    @todo.destroy
+    flash[:notice] = "Todo DESTROYED successfully!"
+    redirect_to project_todos_path(project)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todo.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def todo_params
-      params.require(:todo).permit(:name, :description, :priority, :completed, :project_id)
-    end
+  def set_project
+    @project = current_user.projects.find(params[:project_id])
+  end
+
+  def set_todo
+    @todo = Todo.joins(:project).where(projects: { user_id: current_user.id }).find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:name, :description, :priority, :completed)
+  end
 end
