ca6060e78e203791d7082909c138573047ed84dc
Martin <maguera37@gmail.com>
2026-05-06 16:06:03 -0300

    feat: added set_data method to load user projects and todos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  helper_method :current_user
+  helper_method :current_user, :set_data
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
@@ -11,5 +11,10 @@ class ApplicationController < ActionController::Base
   private
     def current_user
       @current_user ||= User.find_by(id: Current.session.user_id)
+    end
+
+    def set_data
+      @projects = current_user.projects
+      @todos = current_user.todos.includes(:project)
     end
 end
