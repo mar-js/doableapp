302f6f90554fc265fa866d86bb879115ef04d5f9
Martin <maguera37@gmail.com>
2026-05-20 16:25:13 -0300

    fix: updated eagerLoadControllersFrom to use require.context for dynamic loading

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,6 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+
+const context = require.context("./", true, /\.js$/)
+eagerLoadControllersFrom(context, application)
