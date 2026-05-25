f4f2b2ce90ffe55985b8fe0e11c2147afd737656
Martin <maguera37@gmail.com>
2026-05-25 17:04:06 -0300

    fix: removed stale_when_importmap_changes to optimize HTML response handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
-
   private
     def current_user
       @current_user ||= User.find_by(id: Current.session.user_id)
