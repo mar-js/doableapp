b3d2298b27b64bf4b3f034ad87639c33269ac940
Martin <maguera37@gmail.com>
2026-05-29 15:47:58 -0300

    feat: added ForgotPassword and UpdatePassword components to components export

--- app/javascript/react/components/index.js
+++ app/javascript/react/components/index.js
@@ -1,9 +1,13 @@
+import ForgotPassword from "./ForgotPassword"
 import Login from "./Login"
 import Subtitle from "./Subtitle"
 import Title from "./Title"
+import UpdatePassword from "./UpdatePassword"
 
 export const components = {
   Title,
   Subtitle,
-  Login
+  Login,
+  ForgotPassword,
+  UpdatePassword
 }
