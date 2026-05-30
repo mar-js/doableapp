d14ef5d9030a30a3db2eff422d54cdb6fc6ba833
Martin <maguera37@gmail.com>
2026-05-30 16:20:06 -0300

    feat: reorganized component imports and removed unused Title and Subtitle components

--- app/javascript/react/components/index.js
+++ app/javascript/react/components/index.js
@@ -1,13 +1,33 @@
+import AppHeader from "./AppHeader"
+import FlashNotice from "./FlashNotice"
 import ForgotPassword from "./ForgotPassword"
+import Home from "./Home"
 import Login from "./Login"
-import Subtitle from "./Subtitle"
-import Title from "./Title"
+import ProjectEdit from "./ProjectEdit"
+import ProjectNew from "./ProjectNew"
+import ProjectShow from "./ProjectShow"
+import ProjectsIndex from "./ProjectsIndex"
+import Register from "./Register"
+import TodoEdit from "./TodoEdit"
+import TodoNew from "./TodoNew"
+import TodoShow from "./TodoShow"
+import TodosIndex from "./TodosIndex"
 import UpdatePassword from "./UpdatePassword"
 
 export const components = {
-  Title,
-  Subtitle,
+  AppHeader,
+  FlashNotice,
+  Home,
   Login,
+  Register,
   ForgotPassword,
-  UpdatePassword
+  UpdatePassword,
+  ProjectsIndex,
+  ProjectShow,
+  ProjectNew,
+  ProjectEdit,
+  TodosIndex,
+  TodoShow,
+  TodoNew,
+  TodoEdit
 }
