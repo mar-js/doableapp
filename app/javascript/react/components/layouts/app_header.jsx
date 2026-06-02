8a3620c9522bc662334d0fd5fcd98650c30249b6
Martin <maguera37@gmail.com>
2026-06-02 02:08:36 -0300

    refactor: improved the structure of the AppHeader component by removing unnecessary fragments and nesting

--- app/javascript/react/components/layouts/app_header.jsx
+++ app/javascript/react/components/layouts/app_header.jsx
@@ -12,19 +12,16 @@ export default function AppHeader({
   return (
     <header>
       <a href={rootPath}>Doable</a>
-      <br />
       {signedIn && (
-        <>
+        <nav>
+          {showProjectsLink && <a href={projectsPath}>Projects</a>}
+          {showTodosLink && <a href={todosPath}>Todos</a>}
           <form action={signOutPath} method="post" acceptCharset="UTF-8" style={{ display: "inline" }}>
             <input type="hidden" name="_method" value="delete" />
             <input type="hidden" name="authenticity_token" value={csrfToken()} />
             <button type="submit">Sign out</button>
           </form>
-          <nav>
-            {showProjectsLink && <a href={projectsPath}>Projects</a>}
-            {showTodosLink && <a href={todosPath}>Todos</a>}
-          </nav>
-        </>
+        </nav>
       )}
     </header>
   )
