06d0ae320415f73f550841016dafa82230774b57
Martin <maguera37@gmail.com>
2026-06-02 01:28:49 -0300

    feat: added SessionsNew component for user login

---
+++ app/javascript/react/components/sessions/new.jsx
@@ -0,0 +1,43 @@
+import { csrfToken } from "../../utils/csrf"
+
+export default function SessionsNew({
+  action,
+  emailAddress = "",
+  forgotPasswordPath,
+  registerPath
+}) {
+  return (
+    <>
+      <h3>Login</h3>
+      <form action={action} method="post" acceptCharset="UTF-8">
+        <input type="hidden" name="authenticity_token" value={csrfToken()} />
+        <input
+          type="email"
+          name="email_address"
+          required
+          autoFocus
+          autoComplete="username"
+          placeholder="Enter your email address"
+          defaultValue={emailAddress}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          required
+          autoComplete="current-password"
+          placeholder="Enter your password"
+          maxLength={72}
+        />
+        <br />
+        <input type="submit" value="Sign in" />
+      </form>
+      <br />
+      <div>
+        <a href={forgotPasswordPath}>Forgot password?</a>
+        {" | "}
+        <a href={registerPath}>Register</a>
+      </div>
+    </>
+  )
+}
