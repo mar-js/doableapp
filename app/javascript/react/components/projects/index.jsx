6c259c5474e2567a470a372ba660ce2462b3a57d
Martin <maguera37@gmail.com>
2026-06-02 01:28:34 -0300

    feat: added ProjectsIndex component to display a list of projects

---
+++ app/javascript/react/components/projects/index.jsx
@@ -0,0 +1,15 @@
+import ProjectCard from "./card"
+
+export default function ProjectsIndex({ projects, newProjectPath }) {
+  return (
+    <>
+      <h3>Projects</h3>
+      <div id="Projects">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
+      </div>
+      <a href={newProjectPath}>New project</a>
+    </>
+  )
+}
