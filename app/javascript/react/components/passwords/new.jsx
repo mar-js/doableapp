79821a5f3d17c758a9efeaceb37713215060f508
Martin <maguera37@gmail.com>
2026-06-02 01:28:20 -0300

    feat: added PasswordsNew component for password reset functionality

---
+++ app/javascript/react/components/passwords/new.jsx
@@ -0,0 +1,24 @@
+import { csrfToken } from "../../utils/csrf"
+
+export default function PasswordsNew({ action, emailAddress = "", alert }) {
+  return (
+    <>
+      <h3>Forgot your password?</h3>
+      {alert && <div style={{ color: "red" }}>{alert}</div>}
+      <form action={action} method="post" acceptCharset="UTF-8">
+        <input type="hidden" name="authenticity_token" value={csrfToken()} />
+        <input
+          type="email"
+          name="email_address"
+          required
+          autoFocus
+          autoComplete="username"
+          placeholder="Enter your email address"
+          defaultValue={emailAddress}
+        />
+        <br />
+        <input type="submit" value="Email reset instructions" />
+      </form>
+    </>
+  )
+}
