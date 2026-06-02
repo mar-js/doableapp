67cf316f853dc3c1ce7cccd3fba0d010167355bf
Martin <maguera37@gmail.com>
2026-06-02 01:29:32 -0300

    feat: reorganized component imports and exports for better structure

--- app/javascript/react/components/index.js
+++ app/javascript/react/components/index.js
@@ -1,33 +1,33 @@
-import AppHeader from "./AppHeader"
-import FlashNotice from "./FlashNotice"
-import ForgotPassword from "./ForgotPassword"
-import Home from "./Home"
-import Login from "./Login"
-import ProjectEdit from "./ProjectEdit"
-import ProjectNew from "./ProjectNew"
-import ProjectShow from "./ProjectShow"
-import ProjectsIndex from "./ProjectsIndex"
-import Register from "./Register"
-import TodoEdit from "./TodoEdit"
-import TodoNew from "./TodoNew"
-import TodoShow from "./TodoShow"
-import TodosIndex from "./TodosIndex"
-import UpdatePassword from "./UpdatePassword"
+import layoutsAppHeader from "./layouts/app_header"
+import layoutsFlashNotice from "./layouts/flash_notice"
+import pagesHome from "./pages/home"
+import passwordsEdit from "./passwords/edit"
+import passwordsNew from "./passwords/new"
+import projectsEdit from "./projects/edit"
+import projectsIndex from "./projects/index"
+import projectsNew from "./projects/new"
+import projectsShow from "./projects/show"
+import registersNew from "./registers/new"
+import sessionsNew from "./sessions/new"
+import todosEdit from "./todos/edit"
+import todosIndex from "./todos/index"
+import todosNew from "./todos/new"
+import todosShow from "./todos/show"
 
 export const components = {
-  AppHeader,
-  FlashNotice,
-  Home,
-  Login,
-  Register,
-  ForgotPassword,
-  UpdatePassword,
-  ProjectsIndex,
-  ProjectShow,
-  ProjectNew,
-  ProjectEdit,
-  TodosIndex,
-  TodoShow,
-  TodoNew,
-  TodoEdit
+  "layouts/flash_notice": layoutsFlashNotice,
+  "layouts/app_header": layoutsAppHeader,
+  "pages/home": pagesHome,
+  "sessions/new": sessionsNew,
+  "registers/new": registersNew,
+  "passwords/new": passwordsNew,
+  "passwords/edit": passwordsEdit,
+  "projects/index": projectsIndex,
+  "projects/show": projectsShow,
+  "projects/new": projectsNew,
+  "projects/edit": projectsEdit,
+  "todos/index": todosIndex,
+  "todos/show": todosShow,
+  "todos/new": todosNew,
+  "todos/edit": todosEdit
 }
